d73916df9b1b5d83c047dc2ae3e253dcf247a896
狗電腦\USER <gg23456789tw@gmail.com>
2025-01-28 14:04:43 +0800

    set up the rest hbs settings

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const { engine } = require('express-handlebars')
 const app = express();
 const port = 3000;
 
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 app.use(express.static('public'))
+
 //靜態路由
 app.get('/', (req, res) => {
   res.redirect('/restaurants');
@@ -10,7 +15,7 @@ app.get('/', (req, res) => {
 
 //動態路由
 app.get('/restaurants', (req, res) => {
-  res.send('Listing Restaurants');
+  res.render('index');
 });
 
 app.get('/restaurant/:id', (req, res) => {
