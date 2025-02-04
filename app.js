0aff3dd6b54d64371f07ef215789bc5bdda40711
Tim2480 <gg23456789tw@gmail.com>
2025-02-04 16:00:54 +0800

    set up and introduce bootstrap css js & custom style for index

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const { engine } = require('express-handlebars')
+const express = require("express");
+const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+const restaurants = require("./public/jsons/restaurant.json").results;
 
-app.engine('.hbs', engine({ extname: '.hbs' }))
-app.set('view engine', '.hbs')
-app.set('views', './views')
-app.use(express.static('public'))
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+app.use(express.static("public"));
 
 //靜態路由
-app.get('/', (req, res) => {
-  res.redirect('/restaurants');
+app.get("/", (req, res) => {
+  res.redirect("/restaurants");
 });
 
 //動態路由
-app.get('/restaurants', (req, res) => {
-  res.render('index');
+app.get("/restaurants", (req, res) => {
+  res.render("index", {restaurants});
 });
 
-app.get('/restaurant/:id', (req, res) => {
-  const id = req.params.id
-  res.send(`Restaurant id : ${id}`)
-})
+app.get("/restaurant/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Restaurant id : ${id}`);
+});
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`);
